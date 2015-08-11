8fd807b868721feabf393732605c14cc446d6d0f
Flavius <flaav.gbr@gmail.com>
2015-08-11 11:47:14 +0300

    Create script.js

---
+++ livesearch/js/script.js
@@ -0,0 +1,18 @@
+var expectSubscriberNames = function(expectedNames, key) {
+  element.all(by.repeater(key + ' in subscribers').column(key + '.name')).then(function(arr) {
+    arr.forEach(function(wd, i) {
+      expect(wd.getText()).toMatch(expectedNames[i]);
+    });
+  });
+};
+
+it('should search across all fields when filtering with a string', function() {
+  var searchText = element(by.model('searchText'));
+  searchText.clear();
+  searchText.sendKeys('m');
+  expectSubscriberNames(['Mary', 'Mike', 'Adam'], 'subscriber');
+
+  searchText.clear();
+  searchText.sendKeys('76');
+  expectSubscriberNames(['John', 'Julie'], 'subscriber');
+});
