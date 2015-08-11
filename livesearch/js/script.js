82ea192b314d477d30a667d46e50d5c861bba19c
Flavius <flaav.gbr@gmail.com>
2015-08-11 12:22:46 +0300

    Update script.js

--- livesearch/js/script.js
+++ livesearch/js/script.js
@@ -5,14 +5,3 @@ var expectSubscriberNames = function(expectedNames, key) {
     });
   });
 };
-
-it('should search across all fields when filtering with a string', function() {
-  var searchText = element(by.model('searchText'));
-  searchText.clear();
-  searchText.sendKeys('m');
-  expectSubscriberNames(['Mary', 'Mike', 'Adam'], 'subscriber');
-
-  searchText.clear();
-  searchText.sendKeys('76');
-  expectSubscriberNames(['John', 'Julie'], 'subscriber');
-});
