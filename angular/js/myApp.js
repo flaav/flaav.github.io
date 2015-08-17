f9d5c6698f02812649465d5abc96ebb04a778647
Flavius <flaav.gbr@gmail.com>
2015-08-17 11:26:17 +0300

    Update myApp.js

--- angular/js/myApp.js
+++ angular/js/myApp.js
@@ -3,7 +3,7 @@ angular.module('myApp', [])
 .directive('elementDirective', function($log){
     return{
       restrict:'E',
-      template: '<p>The template</p>',
+      template: '<p>Za template</p>',
       link: function(scope, el, attrs){
           $log.log(el.html());
           $log.log(attrs.someAttr);
