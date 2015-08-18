fea04f9b05b9b8f5e70ac7a217c6b02f1fbdbbc3
Flavius <flaav.gbr@gmail.com>
2015-08-18 11:29:26 +0300

    Update myApp.js

--- angular/js/myApp.js
+++ angular/js/myApp.js
@@ -1,17 +1,5 @@
 angular.module('myApp', [])
 
-.directive('attributeDirective', function($log){
-    return{
-      restrict:'A',
-      template: '<p>An attribute directive</p>',
-      link: function(scope, el, attrs){
-          $log.log(el.html());
-          $log.log(attrs.attributeDirective);
-          $log.log(attrs.someAttr);
-      }
-    };
-});
-
 .directive('vectorText', function($document){
    return{
        template: '<span>{{ heading }}</span>',
