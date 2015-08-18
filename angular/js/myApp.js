d95f9c6ede44fc5203d3baa995058717501c1935
Flavius <flaav.gbr@gmail.com>
2015-08-18 11:23:04 +0300

    Update myApp.js

--- angular/js/myApp.js
+++ angular/js/myApp.js
@@ -11,3 +11,36 @@ angular.module('myApp', [])
       }
     };
 });
+
+.directive('vectorText', function($document){
+   return{
+       template: '<span>{{ heading }}</span>',
+       link: function(scope, el, attrs){
+           //init the css
+           el.css({
+               'float': 'left',
+               'padding': attrs.buffers+"px"
+           });
+           
+           //init scope variable
+           scope.heading= '';
+           
+           //set event listener and handler
+           $document.on('mouseover', function(event){
+               //mouseover event does not start $digest
+               scope.$apply(function(){
+                  if(event.pageY < 300){
+                      scope.heading = 'N';
+                  } else{
+                      scope.heading = 'S';
+                  }
+                  if(event.pageX < 300 ){
+                      scope.heading += 'W';
+                  }else{
+                      scope.heading += 'E';
+                  }
+               });
+           });
+       }
+   }; 
+});
