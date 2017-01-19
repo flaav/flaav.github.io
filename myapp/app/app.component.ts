5c232b54bceb5e3818d19509981d238ea949048c
Flavius <flaav.gbr@gmail.com>
2017-01-19 13:45:15 +0200

    Add files via upload

--- myapp/app/app.component.ts
+++ myapp/app/app.component.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}} the Parrot</h1>`
+  template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent { name = 'Garret'; }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+export class AppComponent  { name = 'Angular'; }
